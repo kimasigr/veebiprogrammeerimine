9745ecf66f155ca897903d918ea0185e2f3a52b9
Sigrid Kimask <kimasigr@users.noreply.github.com>
2017-09-12 14:15:26 +0300

    Lisatud on esimene jupp PHP koodi

---
+++ esimene.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Sigrid Kimask veebiprogrammeerimine</title>
+</head>
+<body>
+	<h1>Sigrid Kimask veebiprogrammeerimine</h1>
+	<p>See leht on loodud õppetöö raames ning ei sisalda mingit tõsiseltvõetavat sisu.</p>
+	
+	<?php
+		echo "See on esimene jupp PHP abil väljastatud teksti";
+	?>
+	
+</body>
+</html>
